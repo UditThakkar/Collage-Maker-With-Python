eac648feb2790e57cf900df9360a4fd1e084f9b1
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-09-07 01:59:02 +0530

    Add files via upload

---
+++ collage.py
@@ -0,0 +1,17 @@
+import cv2
+import  PIL 
+from PIL import Image
+import numpy as np
+
+
+def collage_maker(image1, image2, name):
+    i1 = cv2.imread(image1)
+    i2 = cv2.imread(image2)
+    img1 = cv2.resize(i1,(400,400),interpolation = cv2.INTER_LINEAR)
+    img2 = cv2.resize(i2,(400,400),interpolation = cv2.INTER_LINEAR)
+    collage = np.vstack((img1, img2))
+    cv2.imwrite(name,collage)
+
+
+# To Run The Above Function
+collage_maker("image1.jpg", "image2.jpg", "new.jpg")
